e5b447ca28c68af73abf5f208f45a4de15f069f5
mahiru <634131831@qq.com>
2021-12-01 17:01:35 +0800

    [feat]新增查询元素的方法

--- src/main/code/util/SeleniumUtil.py
+++ src/main/code/util/SeleniumUtil.py
@@ -1,5 +1,8 @@
 # Author : lml
 # Date : 2021/12/1
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.wait import WebDriverWait
 from seleniumwire import webdriver
 
 
@@ -19,7 +22,11 @@ class SeleniumUtil:
         options = webdriver.ChromeOptions()
         # options.add_argument('User-Agent=Mozilla/5.0 (Linux; U; Android 8.1.0; zh-cn; BLA-AL00 Build/HUAWEIBLA-AL00) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/57.0.2987.132 MQQBrowser/8.9 Mobile Safari/537.36')
         # 浏览器不提供可视化页面
-        options.add_argument('--headless')
+        # options.add_argument('--headless')
+        # 禁用扩展
+        options.add_argument('disable-extensions')
+        # 禁用阻止弹出窗口
+        options.add_argument('--disable-popup-blocking')
         # 设置这两个参数就可以避免密码提示框的弹出
         options.add_experimental_option('prefs', {
             'credentials_enable_service': False,
@@ -41,9 +48,15 @@ class SeleniumUtil:
         """
         cls.selenium_driver.quit()
 
+    @classmethod
+    def find_element(cls, by, value) -> WebElement:
+        return WebDriverWait(cls.selenium_driver, 10).until(lambda browser: browser.find_elements(by, value))
+
 
 if __name__ == '__main__':
     driver = SeleniumUtil.get_driver()
     driver.get('https://www.baidu.com')
-    SeleniumUtil.get_request_msg(driver)
+    # element = WebDriverWait(driver, 10).until(lambda browser: browser.find_elements(By.ID, 'kw'))
+    elements = SeleniumUtil.find_element(By.ID, 'kw')
+    elements[0].send_keys('selenium')
     SeleniumUtil.close_driver()
