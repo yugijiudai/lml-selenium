0499be136c66747604396697787aef4763bd0d9a
mahiru <634131831@qq.com>
2021-12-22 19:38:50 +0800

    [refactor]优化生成类的代码

--- src/main/code/util/ClassGeneratorUtil.py
+++ src/main/code/util/ClassGeneratorUtil.py
@@ -1,21 +1,16 @@
 # Author : lml
 # Date : 2021/12/22
 
-"""
-文件说明
-"""
-
 
 class ClassGeneratorUtil:
 
     @staticmethod
-    def generate_cls(clz, filepath, params=None):
+    def generate_cls(clz, filepath, params=None) -> None:
         """
         自动生成类和getter setter
         :param clz: 类名字符串
         :param filepath: 路径字符串，最好绝对路径，如果生成在当前目录就写空字符串
         :param params: 类中的属性列表
-        :return:
         """
         if params is None:
             params = []
@@ -27,21 +22,21 @@ class ClassGeneratorUtil:
         lines += ['\t__slots__ = ' + str(tuple([f"__{param}" for param in params])) + "\n"]
         lines += ["\tdef __init__(self):"]
         lines += [f"\t\tself.__{param} = None" for param in params]
-
+        lines += [""]
         for param in params:
             lines += [
                 "\t@property",
                 f"\tdef {param}(self):",
-                "\t\treturn self.__{0}\n".format(param),
-                "\t@{0}.setter".format(param),
-                "\tdef {0}(self, value):".format(param),
-                "\t\tself.__{0} = value\n".format(param)
+                f"\t\treturn self.__{param}\n",
+                f"\t@{param}.setter",
+                f"\tdef {param}(self, value):",
+                f"\t\tself.__{param} = value\n"
             ]
         lines.append("\tdef to_dict(self):")
         lines.append('\t\treturn {')
-        lines += [f'\t\t\t"{p1}": self.__{p2},' for p1, p2 in zip(params, params)]
+        lines += [f'\t\t\t"{param}": self.__{param},' for param in params]
         lines.append('\t\t}')
         lines = [i + "\n" for i in lines]
         with open(filepath, "w+", encoding="utf-8") as fp:
             fp.writelines(lines)
-            print(f"类{clz}生成成功")
+            print(f"类{clz}生成成功,路径{filepath}")
--- src/test/code/util/test_reflect_util.py
+++ src/test/code/util/test_reflect_util.py
@@ -14,4 +14,4 @@ class TestReflectUtil(TestCase):
         print(ReflectUtil.run_clz_method(className='GlobalConfig', model='src.main.code.config.GlobalConfig', methodName='get_config', args=[]))
 
     def test_gen_cls(self):
-        ClassGeneratorUtil.generate_cls("UserEntity", f'{ResourceUtil.get_root_path()}/src/main/code/util/UserEntity.py', params=["name", "pwd", "address"])
+        ClassGeneratorUtil.generate_cls("HandleDto", f'{ResourceUtil.get_root_path()}/src/main/code/dto/HandleDto.py', params=["name", "pwd", "address"])
